0460a1821f1af56300ae175a12119951e0b4fa46
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-07-30 12:31:09 +0530

    Just learnt the way of connecting to the database a different way.

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://shyamsunder:admin1234@contact-book-cluster.kix2je8.mongodb.net/myHandbook-backend?retryWrites=true&w=majority";
 
 const connectToMongo = async () => {
-  try {
-          const connect = await mongoose.connect(mongoURI);
-          console.log("Connected to ",connect.connection.name ,"and the host is ", connect.connection.host);
-      } catch (err) {
-          console.log(err);
-          process.exit(1);
-      }
+//   try {
+        //   const connect =
+          await mongoose.connect(mongoURI);
+        //   console.log("Connected to ",connect.connection.name ,"and the host is ", connect.connection.host);
+        console.log("Success");
+    //   } catch (err) {
+    //       console.log(err);
+    //       process.exit(1);
+    //   }
 }
 
 
