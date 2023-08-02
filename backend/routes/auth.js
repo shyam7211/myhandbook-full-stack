2276033963fbf762fbdb9a3ce04d8eddf142cf86
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-08-02 20:06:37 +0530

    Hashing of the password by bcryptjs and the access token for the registered user was created.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/UserModel");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const ACCESS_TOKEN_KEY = '1@3$5^7*9)-+';
 
 //Create a user using: POST "/api/auth/createuser". Doesn't require authentication
 router.post("/createuser", [
@@ -24,9 +28,20 @@ router.post("/createuser", [
                 return res.status(400).json({message: "User already exists with this mail."})
             }
             // Creating the user.
-            user = await User.create(req.body);
+            const salt = await bcrypt.genSalt(10);
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: await bcrypt.hash(req.body.password, salt)
+            });
+
+            const accessToken = jwt.sign({user:{
+                user_id: user.id,
+                user_email: user.email
+            }}, ACCESS_TOKEN_KEY);
+            
             console.log(user);
-            res.status(201).json({ message: 'User created successfully', user: user });
+            res.status(201).json({ message: 'User created successfully', user, accessToken});
         } catch (error) {
             console.error(error.message);
             res.status(500).json({ error: 'Failed to create user' });
