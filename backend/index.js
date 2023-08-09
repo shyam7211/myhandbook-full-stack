96310beb40caf35c3e5f78a5c031eef5cad56f6f
Shyam Sunder Prajapat <shyamsunderprajapat.nita@gmail.com>
2023-08-09 16:40:18 +0530

    Added cors in the backend for connecting from all origins and letting our client & server connect together.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express')
+const cors = require('cors');
 
 connectToMongo();
 const app = express()
-const port = 5000
+const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
